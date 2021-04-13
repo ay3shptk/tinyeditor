2ca1cd6b044515461b810668585572bacf73ce95
Ayush Pathak <62694274+ayshptk@users.noreply.github.com>
2021-04-13 14:17:45 +0530

    uhmm

--- tiny.js
+++ tiny.js
@@ -10,7 +10,7 @@ function compile() {
 	    code.open();
 		code.writeln(decodeURIComponent(input.value));
 		code.close();
-		console.log("https://tinycode.aysh.me/?input=" + encodeURIComponent(input.value))
+		console.log("https://tinycode.aysh.me/?input=" + input.value)
 
       };
     };
