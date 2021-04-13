bfae24a1bfec29b586a174588c8747d76c9afa5e
Ayush Pathak <62694274+ayshptk@users.noreply.github.com>
2021-04-13 14:07:46 +0530

    Trying sharing options

--- tiny.js
+++ tiny.js
@@ -1,3 +1,8 @@
+var varSection = window.location.search.substr(1);
+var varArray = varSection.split("&");
+for(var v=0; v<varArray.length; v++) { var keyValueArray = varArray[v].split("=");}
+console.log (keyValueArray)
+if(keyValueArray[0]=="input") { varValue=keyValueArray[1]; document.getElementById("input").value=varValue;}
 function compile() {
 	var input = document.getElementById("input");
 	var code = document.getElementById("code").contentWindow.document;
@@ -6,5 +11,6 @@ function compile() {
 		code.writeln(input.value);
 		code.close();
       };
+	console.log("https://tinycode.aysh.me/?input=" + input.value + "/")
     };
 compile();
