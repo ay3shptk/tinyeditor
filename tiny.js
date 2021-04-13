fa23524104cfd53c7de0bd22b4cebc06dde0d903
Ayush Pathak <62694274+ayshptk@users.noreply.github.com>
2021-04-13 14:30:26 +0530

    FUCKK

--- tiny.js
+++ tiny.js
@@ -2,15 +2,15 @@ var varSection = window.location.search.substr(1);
 var varArray = varSection.split("&");
 for(var v=0; v<varArray.length; v++) { var keyValueArray = varArray[v].split("=");}
 console.log (keyValueArray)
-if(keyValueArray[0]=="input") { varValue=keyValueArray[1]; document.getElementById("input").value=varValue;}
+if(keyValueArray[0]=="input") { varValue=decodeURIComponent(keyValueArray[1]); document.getElementById("input").value=varValue;}
 function compile() {
 	var input = document.getElementById("input");
 	var code = document.getElementById("code").contentWindow.document;
     document.body.onkeyup = function(){
 	    code.open();
-		code.writeln(decodeURIComponent(input.value));
+		code.writeln(input.value);
 		code.close();
-		console.log("https://tinycode.aysh.me/?input=" + input.value)
+		console.log("https://tinycode.aysh.me/?input=" + encodeURIComponent(input.value))
 
       };
     };
