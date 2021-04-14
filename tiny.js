d06e8b517de723b028921aa83cecb3b8ae3f616b
Ayush Pathak <62694274+ayshptk@users.noreply.github.com>
2021-04-14 12:36:54 +0530

    a bit of this a bit of that

--- tiny.js
+++ tiny.js
@@ -3,18 +3,19 @@ var varArray = varSection.split("&");
 for(var v=0; v<varArray.length; v++) { var keyValueArray = varArray[v].split("=");}
 console.log (keyValueArray)
 if(keyValueArray[0]=="input") { varValue=decodeURIComponent(keyValueArray[1]); document.getElementById("input").value=varValue;}
+if(keyValueArray[2]=="style") { varValue=decodeURIComponent(keyValueArray[3]); document.getElementById("style").value=varValue;}
+if(keyValueArray[4]=="script") { varValue=decodeURIComponent(keyValueArray[5]); document.getElementById("script").value=varValue;}
 function compile() {
 	var input = document.getElementById("input");
 	var code = document.getElementById("code").contentWindow.document;
 	code.open();
-	code.writeln(input.value);
+	code.writeln("<style>" + style.value + "</style><script>" + script.value + "</script>" + input.value );
 	code.close();
     document.body.onkeyup = function(){
 		code.open();
-		code.writeln(input.value);
+		code.writeln("<style>" + style.value + "</style><script>" + script.value + "</script>" + input.value );
 		code.close();
-		console.log("https://tinycode.aysh.me/?input=" + encodeURIComponent(input.value))
-
       };
     };
 compile();
+
